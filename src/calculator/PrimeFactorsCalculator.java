1704510d0750b392f8386b1fde55b9c8a8a7280c
hjliu <hjliu@thoughtworks.com>
2015-10-15 15:49:07 +0800

    add test for prime numbers with more than one prime factors

--- src/calculator/PrimeFactorsCalculator.java
+++ src/calculator/PrimeFactorsCalculator.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactorsCalculator {
-    public int[] primeFactors(int number) {
-        return new int[]{number};
+    public List<Integer> primeFactors(int number) {
+        final ArrayList<Integer> result = new ArrayList<>();
+        if (!isPrime(number)) {
+            for (int i = 2; i <= number; i++) {
+                if (isPrime(i) && number % i == 0) {
+                    result.add(i);
+                    while (number % i == 0) {
+                        number = number / i;
+                    }
+                }
+            }
+        } else {
+            result.add(number);
+        }
+        return result;
+    }
+
+    private boolean isPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- test/calculator/PrimeFactorsCalculatorTest.java
+++ test/calculator/PrimeFactorsCalculatorTest.java
@@ -1,27 +1,51 @@
 package calculator;
 
-import org.hamcrest.collection.IsArray;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PrimeFactorsCalculatorTest {
 
     private final PrimeFactorsCalculator calc = new PrimeFactorsCalculator();
+    private final ArrayList<Integer> integers = new ArrayList<>();
 
     @Test
     public void should_get_2_for_2() throws Exception {
-        assertThat(calc.primeFactors(2),equalTo(new int[]{2}));
+        Collections.addAll(integers, 2);
+        assertThat(calc.primeFactors(2), equalTo(integers));
     }
 
     @Test
     public void should_get_3_for_3() throws Exception {
-        assertThat(calc.primeFactors(3),equalTo(new int[]{3}));
+        Collections.addAll(integers, 3);
+        assertThat(calc.primeFactors(3), equalTo(integers));
     }
 
+    @Test
+    public void should_get_3_for_9() throws Exception {
+        Collections.addAll(integers, 3);
+        assertThat(calc.primeFactors(9), equalTo(integers));
+    }
+
+    @Test
+    public void should_get_2_for_8() throws Exception {
+        Collections.addAll(integers, 2);
+        assertThat(calc.primeFactors(8), equalTo(integers));
+    }
+
+    @Test
+    public void should_get_2_3_for_6() throws Exception {
+        Collections.addAll(integers, 2, 3);
+        assertThat(calc.primeFactors(6), equalTo(integers));
+    }
+
+    @Test
+    public void should_get_3_5_for_45() throws Exception {
+        Collections.addAll(integers, 3, 5);
+        assertThat(calc.primeFactors(45), equalTo(integers));
+    }
 }
