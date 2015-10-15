a731cba2c39ff7a5e525a521a8180c629d394feb
hjliu <hjliu@thoughtworks.com>
2015-10-15 16:31:07 +0800

    refactor prime factors structure again

--- src/calculator/PrimeFactorsCalculator.java
+++ src/calculator/PrimeFactorsCalculator.java
@@ -1,30 +1,24 @@
 package calculator;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class PrimeFactorsCalculator {
     public List<Integer> primeFactors(int number) {
         final ArrayList<Integer> result = new ArrayList<>();
-        int i = number;
-        while (i>1){
+        for (int i = 2; i <= number; i++) {
             if (isPrime(i) && number % i == 0) {
                 result.add(i);
                 while (number % i == 0) {
                     number = number / i;
                 }
-                i=number;
-            }else {
-                i--;
             }
         }
-        Collections.reverse(result);
         return result;
     }
 
     private boolean isPrime(int number) {
-        for (int i = 2; i < number; i++) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
                 return false;
             }
